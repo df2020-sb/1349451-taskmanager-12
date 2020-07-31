e0744e54b51a21e6b314fbc29c06ea2a41d11f2c
Denis Fomin <denisfomin2020@gmail.com>
2020-07-31 20:16:48 +0300

    Учебный проект: время разбивать камни

---
+++ webpack.config.js
@@ -0,0 +1,18 @@
+const path = require(`path`);
+
+module.exports = {
+  context: path.resolve(__dirname, 'src'),
+  mode: `development`,
+  entry: `./main.js`,
+  output: {
+    filename: `bundle.js`,
+    path: path.join(__dirname, `public`),
+  },
+
+  devtool: `source-map`,
+  devServer: {
+    port: 5050,
+    contentBase: path.join(__dirname, `public`),
+    hot: true,
+  },
+};
