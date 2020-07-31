bb348ac257cc26232ccdf724810151b607163808
Denis Fomin <denisfomin2020@gmail.com>
2020-07-31 22:37:48 +0300

    Исправления

--- webpack.config.js
+++ webpack.config.js
@@ -1,18 +1,18 @@
-const path = require(`path`);
+const path = require('path');
 
 module.exports = {
   context: path.resolve(__dirname, 'src'),
-  mode: `development`,
-  entry: `./main.js`,
+  mode: 'development',
+  entry: './main.js',
   output: {
-    filename: `bundle.js`,
-    path: path.join(__dirname, `public`),
+    filename: 'bundle.js',
+    path: path.join(__dirname, 'public'),
   },
 
-  devtool: `source-map`,
+  devtool: 'source-map',
   devServer: {
     port: 5050,
-    contentBase: path.join(__dirname, `public`),
+    contentBase: path.join(__dirname, 'public'),
     hot: true,
   },
 };
